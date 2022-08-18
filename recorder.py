410a944deab7a42c12c26a4fe3affa59437ebc0b
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-08-17 22:10:31 -0300

    1708222210

--- recorder.py
+++ recorder.py
@@ -6,7 +6,8 @@ import wavio as wv
 freq = 44100
 duration = 5
 recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
-                   
+              
+  
 sd.wait() 
 write("recording0.wav", freq, recording) 
 wv.write("recording1.wav", recording, freq, sampwidth=2)
