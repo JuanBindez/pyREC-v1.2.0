5c6bca0f85adaa0c2c81a87aaed91ed5a57c59c6
JuanBindez <juanbindez780@gmail.com>
2022-08-17 21:43:54 -0300

    1708222143

---
+++ recorder.py
@@ -0,0 +1,13 @@
+import sounddevice as sd 
+from scipy.io.wavfile import write 
+import wavio as wv 
+
+
+freq = 44100
+duration = 5
+recording = sd.rec(int(duration * freq),  
+                   samplerate=freq, channels=2) 
+                   
+sd.wait() 
+write("recording0.wav", freq, recording) 
+wv.write("recording1.wav", recording, freq, sampwidth=2)
