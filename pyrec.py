a574da694de7b76397a28f11e75104963a843ae9
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-09-17 22:51:15 -0300

    Update pyrec.py

--- pyrec.py
+++ pyrec.py
@@ -1,3 +1,30 @@
+'''
+MIT License
+
+Copyright (c) 2022 Juan Carlos Bindez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Author: https://github.com/JuanBindez
+'''
+
+
 import sounddevice as sd 
 from scipy.io.wavfile import write 
 import wavio as wv
@@ -33,11 +60,12 @@ def banner():
         | .__/ \__, |_| \_\_____\____|
         |_|    |___/                   v1.0.0
                                
-           
-        
-        
+        Copyright (c) 2022 Juan Carlos Bindez
+
+         *[Ctrl + C] Para Sair do Programa
         '''
     + Color.RESET)
+
 
 def aguardo():
     x = 0
@@ -65,3 +93,7 @@ sd.wait()
 aguardo()
 write("recording0.wav", freq, recording) 
 wv.write("recording1.wav", recording, freq, sampwidth=2)
+
+os.system("clear")
+banner()
+print(Color.VERMELHO + "            GRAVAÇÃO CONCLUÍDA!" + Color.RESET)
